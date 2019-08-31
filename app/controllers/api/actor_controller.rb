72328f668759b0b5a76c8b51c9975cf901d7b70e
jackie <jacqueline.kartchner@gmail.com>
2019-08-30 18:39:21 -0700

    adds params

--- app/controllers/api/actor_controller.rb
+++ app/controllers/api/actor_controller.rb
@@ -1,6 +1,23 @@
 class Api::ActorController < ApplicationController
-  def first_actor_method
-    @actor = Actor.find_by(id:1)
-    render 'first_actor.json.jb'
+
+  def index
+    @actor = Actor.first
+    render 'index.json.jb'
+  end
+
+  def show
+    @actor = Actor.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @actor = Actor.new(
+        id: params[:id],
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        known_for: params[:known_for],
+      )
+    @actor.save
+    render 'create.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/first_actor_url" => "actorss#first_actor_method"
-    get "/all_movies_url" => "movies#all_movies_method"
-    get "/first_movie_url" => "movies#first_movie_method"
-    get "/second_movie_url" => "movies#second_movie_method"
-    get "/third_movie_url" => "movies#third_movie_method"
+    get "/actor/:id" => "actor#show"
+    get "/actor" => "actor#index"
+    post "/actor" => "actor#create"
   end
 end
